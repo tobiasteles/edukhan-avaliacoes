5f177e34736970b3e1e935c8577ac59c11b4d651
tobiasteles <106561230+tobiasteles@users.noreply.github.com>
2026-04-13 13:09:42 -0300

    feat: adicionar novo ID de administrador e ajustar formatação do código

--- lib/admin.ts
+++ lib/admin.ts
@@ -1,18 +1,19 @@
 import { auth } from "@clerk/nextjs/server";
 
 const adminIds = [
-    "user_37UdggT9NxfKCEdpDJ0bIpmcYbw",
-    "user_3B5xJWQ0MleZXmnEPyiUuvtO0ka",
-    "user_39a6wm9NORCQPTpD9CVmCEX27R3",
-"user_3BduBaVYwsgkctuAzatWgxcrp7V"
-]
+  "user_37UdggT9NxfKCEdpDJ0bIpmcYbw",
+  "user_3B5xJWQ0MleZXmnEPyiUuvtO0ka",
+  "user_39a6wm9NORCQPTpD9CVmCEX27R3",
+  "user_3BduBaVYwsgkctuAzatWgxcrp7V",
+  "user_3CJGbtilZ5sk41HIhuDVUJ8sgNb",
+];
 
 export const getIsAdmin = async () => {
-    const { userId } = await auth();
+  const { userId } = await auth();
 
-    if (!userId) {
-        return false;
-    }
+  if (!userId) {
+    return false;
+  }
 
-    return adminIds.indexOf(userId) !== -1;
-}
+  return adminIds.indexOf(userId) !== -1;
+};
